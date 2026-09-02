b42c745f56a5a1c2f93c4bd5a4b17aab69dcc324
FARHAN7700 <farhankhan10470@gmail.com>
2026-09-02 16:34:46 +0530

    chngs

---
+++ V_54/script.js
@@ -0,0 +1,8 @@
+alert("Hello, World!");
+
+console.log("This is a test message.");
+var a=prompt("Enter a number:");
+
+var b=confirm("Do you want to continue?");
+console.log("You entered:", a);
+console.log("Continue?", b);
